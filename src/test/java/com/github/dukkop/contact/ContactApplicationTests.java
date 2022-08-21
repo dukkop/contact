771368d13a1cfbee032fadf227a073fddd39260a
dukkop <dukkop@pm.me>
2022-08-21 03:54:48 -0400

    first commit

---
+++ src/test/java/com/github/dukkop/contact/ContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dukkop.contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
